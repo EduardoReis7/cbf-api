d3fdd5facb20355fa6956b08304ae2e0c838f133
EduardoReis7 <53585326+EduardoReis7@users.noreply.github.com>
2020-11-04 12:38:49 -0300

    commit inicial

---
+++ src/test/java/com/er/cbfapi/CbfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.er.cbfapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbfApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
